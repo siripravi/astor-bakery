d251d95b1322bed4019584722d6cfbf4d178e764
provdigi <purnachandra.valluri@gmail.com>
2024-03-30 13:26:49 +0530

    twn

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
-//import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), tailwind()],
   site: "http://localhost:4321",
 
   vite: {
--- src/components/Index/SiripraviEcommerceFrontendBlocksCategoryBlock.vue
+++ src/components/Index/SiripraviEcommerceFrontendBlocksCategoryBlock.vue
@@ -2,25 +2,44 @@
   <h3>List of Categories</h3>
   <div v-if="block">
     <section class="container mx-auto bg-white">
-      <div class="row">
+      <div
+        class="grid gap-2 px-2 pt-2 pb-2 lg:px-0 xl:px-0 md:px-0 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 xs:grid-cols-3"
+      >
         <template v-for="category in block.extras.elements.categories">
           <div v-if="category.slug" :key="category.id">
-            <div class="card" style="width: 18rem">
-              <img
-                v-bind:src="category.imageSrc.source"
-                class="card-img-top img-fluid"
-              />
-
-              <div class="card-body">
-                <h5 class="card-title">{{ category.name }}</h5>
-                <p class="card-text">{{ category.teaser }}</p>
-
-                <a
-                  class="btn btn-primary"
-                  :href="`/category/${category.id}/${category.slug}`"
-                  >View All</a
-                >
-              </div>
+            <div
+              class=" max-w-md mx-auto rounded-md overflow-hidden shadow-md hover:shadow-lg"
+            >
+              <a
+                class="text-black cursor-pointer hover:underline"
+                :href="`/category/${category.id}/${category.slug}`"
+              >
+                <div class="relative">
+                  <img v-bind:src="category.imageSrc.source" class="w-full" />
+                 
+                  <div
+                    class="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium"
+                  >
+                    SALE
+                  </div>
+                </div>
+                <div class="p-4">
+                  <h3 class="text-lg font-medium mb-2">{{ category.name }}</h3>
+                  <p class="text-gray-600 text-sm mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Duis vitae ante vel eros fermentum faucibus sit amet euismod
+                    lorem.
+                  </p>
+                  <div class="flex items-center justify-between">
+                    <span class="font-bold text-lg">$19.99</span>
+                    <button
+                      class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                    >
+                      Buy Now
+                    </button>
+                  </div>
+                </div>
+              </a>
             </div>
           </div>
         </template>
